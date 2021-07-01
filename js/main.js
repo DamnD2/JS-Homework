fe5a3b59b6b9c3243d5c3d5278f02a3e662238b3
Vitalii <vitalya_daineko@mail.ru>
2021-07-01 16:26:43 +0300

    add middle element check

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@ function getIndex ( array, elem){
 	let difference = 0;
 	return binarySearch (array);
 	function binarySearch (arr) {
+		console.log("PROHOD");
 		if(arr.length === 1){
 			return (arr[0] == elem) ? difference : "element isn't in the array";
 		} else{
 			const middleIndex = Math.floor(arr.length/2);
-			if(elem >= arr[middleIndex]){
+			if(elem === arr[middleIndex]){
+				return difference += middleIndex;
+			}else if(elem >= arr[middleIndex]){
 				difference += middleIndex;
 				return binarySearch(arr.splice(middleIndex));
 			} else {
@@ -16,5 +19,5 @@ function getIndex ( array, elem){
 	}
 }
 
-console.log(getIndex([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21], 20));
-console.log(getIndex([-16,-7,-3,-2,-1,0,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], 24));
+console.log(getIndex([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21], 12));
+console.log(getIndex([-16,-7,-3,-2,-1,0,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], -8));
