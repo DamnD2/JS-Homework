77f9af1c8e977ba9bcfc0696311dd4288d4a9801
Vitalii <vitalya_daineko@mail.ru>
2021-07-01 15:37:53 +0300

    added
    closure

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
-function getIndex ( arr, elem, counter = 0){
-	if(arr.length === 1){
-		return (arr[0] == elem) ? counter : "element isn't in the array";
-	} else{
-		const middleIndex = Math.floor(arr.length/2);
-		if(elem >= arr[middleIndex]){
-			return getIndex(arr.splice(middleIndex), elem, counter+middleIndex);
-		} else {
-			return getIndex(arr.splice(0, middleIndex), elem, counter);
+function getIndex ( array, elem){
+	let difference = 0;
+	return binarySearch (array);
+	function binarySearch (arr) {
+		if(arr.length === 1){
+			return (arr[0] == elem) ? difference : "element isn't in the array";
+		} else{
+			const middleIndex = Math.floor(arr.length/2);
+			if(elem >= arr[middleIndex]){
+				difference += middleIndex;
+				return binarySearch(arr.splice(middleIndex));
+			} else {
+				return binarySearch(arr.splice(0, middleIndex));
+			}
 		}
 	}
 }
 
-console.log(getIndex([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21], 43));
-console.log(getIndex([-16,-7,-3,-2,-1,0,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], -3));
+console.log(getIndex([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21], 20));
+console.log(getIndex([-16,-7,-3,-2,-1,0,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], 24));
